b5f8273f7e6986afc148cc1ad9a14c17ec683770
Helmo Silva <helmo@h9jet.com>
2021-07-05 00:42:44 +0100

    sem o link

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Link from "next/link";
 
 function Home() {
     return (
         <div>
             <h1>Pagina Inicial</h1>
-            <Link href="/sobre">
-                <a>Acessar página Sobre</a>
-            </Link>
+                <a href="/sobre">Acessar página Sobre</a>
             
         </div>
 
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,12 +1,9 @@
-import Link from "next/link";
-
 function Sobre() {
     return (
         <div>
             <h1>Pagina Inicial</h1>
-            <Link href="/">
-                <a>Acessar página Home</a>
-            </Link>
+                <a href="/">Acessar página Home</a>
+            
             
         </div>
 
