fdc3065aec87ea52820310d0a4f767ea12842533
Helmo Silva <helmo@h9jet.com>
2021-07-05 02:31:14 +0100

    adicionando logs

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,4 +1,5 @@
 function Tempo(props) {
+    console.log('> Passando pelo Frontend;')
     const dynamicDate = new Date();
     const dynamicDateStrimg = dynamicDate.toGMTString();
 
@@ -12,6 +13,7 @@ function Tempo(props) {
 }
 
 export  function getStaticProps(){
+    console.log('> Passando pelo getStaticProps();')
     const staticDate = new Date();
     const staticDateStrimg = staticDate.toGMTString();
 
