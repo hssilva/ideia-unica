6bf1a91dc3fe5849710731b8c1301a93b394372d
Helmo Silva <helmo@h9jet.com>
2021-07-06 02:10:54 +0100

    adicionado regiao na api

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,7 +1,14 @@
-function tempo(request, response){
+async function tempo(request, response){
     const dynamic = new Date();
+
+    const regiaoResponse = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/regioes/{macrorregiao}")
+    const regiaoJson = await regiaoResponse.json();
+    const regiao = regiaoJson.nome;
+
+
     response.json({
-        date: dynamic.toGMTString()
+        date: dynamic.toGMTString(),
+        regiao: regiao
     });
 }
 
