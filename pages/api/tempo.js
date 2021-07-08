ca74e15db301a98b7da087109d2d8013658abbce
Helmo Silva <helmo@h9jet.com>
2021-07-08 22:41:42 +0100

    adicionado chache na api

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,15 +2,15 @@ async function tempo(request, response){
     const dynamic = new Date();
     const apiMacrorRegiao = process.env.MACRO_REGIAO;
 
-    const regiaoResponse = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${apiMacrorRegiao}`)
+    /*  const regiaoResponse = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${apiMacrorRegiao}`)
     const regiaoJson = await regiaoResponse.json();
-    const regiao = regiaoJson.nome;
+    const regiao = regiaoJson.nome;*/
 
     response.setHeader('Cache-Contol', 's-maxage=10, stale-while-revalidate');
 
     response.json({
-        date: dynamic.toGMTString(),
-        regiao: regiao
+        date: dynamic.toGMTString()/*,
+        regiao: regiao*/
     });
 }
 
