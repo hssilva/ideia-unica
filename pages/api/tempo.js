cddd1e0420b1c63d3fa40c65d74bbd25d516d1f7
Helmo Silva <helmo@h9jet.com>
2021-07-09 01:50:28 +0100

    alterada a cache p/ 20s

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,16 +1,16 @@
 async function tempo(request, response){
-    const dynamic = new Date();
     const apiMacrorRegiao = process.env.MACRO_REGIAO;
+    const dynamic = new Date();
 
-    /*  const regiaoResponse = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${apiMacrorRegiao}`)
+    const regiaoResponse = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${apiMacrorRegiao}`)
     const regiaoJson = await regiaoResponse.json();
-    const regiao = regiaoJson.nome;*/
+    const regiao = regiaoJson.nome;
 
     response.setHeader('Cache-Contol', 's-maxage=20, stale-while-revalidate');
 
     response.json({
-        date: dynamic.toGMTString()/*,
-        regiao: regiao*/
+        date: dynamic.toGMTString(),
+        regiao: regiao
     });
 }
 
